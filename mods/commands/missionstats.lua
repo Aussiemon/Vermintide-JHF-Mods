7fb57e7c1e43185a599170da7a331adfb6ea116d
Aussiemon <mattrohrlach+github@gmail.com>
2017-05-19 21:06:36 -0600

    Missionstats improvements
    Last Stand stat lookups, better error handling, new send_all location

--- mods/commands/missionstats.lua
+++ mods/commands/missionstats.lua
@@ -18,30 +18,41 @@
 
 local command_name = "missionstats"
 
-if Managers.player == nil then
-	EchoConsole("Please wait before attempting command: " .. command_name .. "!")
+if not Managers.player then
+	EchoConsole("Please wait: stats not yet loaded!")
 	return
 end
 
 local local_player = Managers.player:local_player()
-if local_player == nil then
-	EchoConsole("Please wait before attempting command: " .. command_name .. "!")
+if not local_player then
+	EchoConsole("Please wait: stats not yet loaded!")
 	return
 end
 
 local stat_db = Managers.player:statistics_db()
+if not stat_db then
+	EchoConsole("Please wait: stats not yet loaded!")
+	return
+end
+
 local stats_id = local_player.stats_id(local_player)
+if not stats_id then
+	EchoConsole("Please wait: stats not yet loaded!")
+	return
+end
+
 local this_completion = 0
 local total_completed = 0
 local translated_name = ""
 local user_name = ""
 
-local send_all = false
+local send_all = false -- Change to false to never publicly display level completion stats
 if Managers.chat then
-	send_all = true -- Change to false to never publicly display level completion stats
 	if local_player._cached_name ~= nil then
 		user_name = local_player._cached_name
 	end
+else
+	send_all = false
 end
 
 if send_all then
@@ -136,17 +147,54 @@ for _, level_name in ipairs(UnlockableLevels) do
 	this_completion = stat_db.get_persistent_stat(stat_db, stats_id, "completed_levels", level_name)
 	total_completed = total_completed + this_completion
 	
+	local survival_level_name = nil
+	local this_total_kills = nil
+	local this_veteran_waves = nil
+	local this_champion_waves = nil
+	local this_heroic_waves = nil
+	
+	if level_name == "dlc_survival_ruins" then
+		survival_level_name = "ruins"
+	elseif level_name == "dlc_survival_magnus" then
+		survival_level_name = "magnus"
+	end
+	
+	if survival_level_name then
+		this_total_kills = 
+			stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_hard_kills")) 
+			+ stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_harder_kills")) 
+			+ stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_hardest_kills"))
+		this_veteran_waves = 
+			stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_hard_waves")) 
+		this_champion_waves =
+			stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_harder_waves")) 
+		this_heroic_waves =
+			stat_db.get_persistent_stat(stat_db, stats_id, ("survival_dlc_survival_" .. survival_level_name .. "_survival_hardest_waves"))
+	end
+	
 	if send_all then
-		Managers.chat:send_system_chat_message(1, (translated_name .. ": " .. this_completion), 0, true)
+		if not survival_level_name then
+			Managers.chat:send_system_chat_message(1, (translated_name .. ": " .. this_completion), 0, true)
+		else
+			Managers.chat:send_system_chat_message(1, (translated_name .. ": " .. this_total_kills .. " kills, (" .. this_veteran_waves .. ", " .. this_champion_waves .. ", " .. this_heroic_waves .. ") best waves"), 0, true)
+		end
 	else
-		EchoConsole(translated_name .. ": " .. this_completion)
+		if not survival_level_name then
+			EchoConsole(translated_name .. ": " .. this_completion)
+		else
+			EchoConsole(translated_name .. ": " .. this_total_kills .. " kills, (" .. this_veteran_waves .. ", " .. this_champion_waves .. ", " .. this_heroic_waves .. ") best waves")
+		end
 	end
 end
 
+local total_badges = stat_db.get_persistent_stat(stat_db, stats_id, "endurance_badges")
+
 if send_all then
 	Managers.chat:send_system_chat_message(1, ("Total Completions: " .. total_completed), 0, true)
+	Managers.chat:send_system_chat_message(1, ("Total Endurance Badges: " .. total_badges), 0, true)
 	Managers.chat:send_system_chat_message(1, ("------------------------------"), 0, true)
 else
 	EchoConsole("Total Completions: " .. total_completed)
+	EchoConsole("Total Endurance Badges: " .. total_badges)
 	EchoConsole("------------------------------")
 end
