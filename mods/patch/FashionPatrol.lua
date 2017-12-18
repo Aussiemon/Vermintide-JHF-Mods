d716858ab18ac9fa42a8f4a65bb2b94d33466f68
Matt <mattrohrlach+github@gmail.com>
2017-12-18 14:21:18 -0700

    Updated FashionPatrol to use better mechanism for color change

--- mods/patch/FashionPatrol.lua
+++ mods/patch/FashionPatrol.lua
@@ -41,15 +41,23 @@ FashionPatrol = {
 
 local mod = FashionPatrol
 
-mod.original_storm_variation = {
-		max = UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.max,
-		min = UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.min
+mod.white_storm_variation = {
+	material_variations = {
+		cloth_tint = {
+			min = 31,
+			max = 31,
+			variable = "gradient_variation",
+			materials = {
+				"mtr_outfit"
+			},
+			meshes = {
+				"g_stormvermin_armor_lod0",
+				"g_stormvermin_armor_lod1",
+				"g_stormvermin_armor_lod2"
+			}
+		}
 	}
-
-mod.new_storm_variation = { -- Variation gradient values above 30 are treated as pure white
-		max = 31,
-		min = 31
-	}
+}
 
 -- ##########################################################
 -- ################## Functions #############################
@@ -57,6 +65,42 @@ mod.new_storm_variation = { -- Variation gradient values above 30 are treated as
 mod.create_options = function()
 	Mods.option_menu:add_group("fashion_patrol", "Fashion Patrol")
 	Mods.option_menu:add_item("fashion_patrol", mod.SETTINGS.ACTIVE, true)
+end
+
+mod.trigger_stormvermin_variation = function(unit)
+	local variation_settings = mod.white_storm_variation
+	local variation = variation_settings.material_variations["cloth_tint"]
+
+	local variable_value = math.random(variation.min, variation.max)
+	local variable_name = variation.variable
+	local materials = variation.materials
+	local meshes = variation.meshes
+	local num_materials = #materials
+	
+	local Mesh_material = Mesh.material
+	local Material_set_scalar = Material.set_scalar
+
+	if not meshes then
+		local Unit_set_scalar_for_material_table = Unit.set_scalar_for_material_table
+		Unit_set_scalar_for_material_table(unit, materials, variable_name, variable_value)
+	else 
+		for i=1, #meshes do
+			local mesh = meshes[i]
+			local Unit_mesh = Unit.mesh
+			local current_mesh = Unit_mesh(unit, mesh)
+
+			for material_i=1, num_materials do
+				local material = materials[material_i]
+				local current_material = Mesh_material(current_mesh, material)
+				Material_set_scalar(current_material,variable_name,variable_value)
+			end
+		end
+	end
+	
+	local Unit_set_visibility = Unit.set_visibility
+	Unit_set_visibility(unit, "all", false)
+	
+	return
 end
 
 local get = function(data)
@@ -68,33 +112,19 @@ local save = Application.save_user_settings
 -- ##########################################################
 -- #################### Hooks ###############################
 
-Mods.hook.set(mod_name, "UnitSpawner.spawn_network_unit", function(func, self, unit_name, unit_template_name, extension_init_data, position, rotation, material)
+Mods.hook.set(mod_name, "UnitSpawner.spawn_local_unit_with_extensions", function (func, self, unit_name, unit_template_name, extension_init_data, ...)
+	local unit, unit_template_name = func(self, unit_name, unit_template_name, extension_init_data, ...)
 	
+	-- Changes here: --------------------------------
 	if get(mod.SETTINGS.ACTIVE) then
-		-- Identify Stormvermin patrol unit and change colors
-		local changed_color = false
+		
 		if extension_init_data and extension_init_data.ai_group_system and extension_init_data.ai_group_system.template == "storm_vermin_formation_patrol" then
-			UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.max = mod.new_storm_variation.max
-			UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.min = mod.new_storm_variation.min
-			changed_color = true
+			mod.trigger_stormvermin_variation(unit)
 		end
-		
-		-- Original function
-		local unit, go_id = func(self, unit_name, unit_template_name, extension_init_data, position, rotation, material)
-		
-		-- Restore color settings
-		if changed_color then
-			UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.max = mod.original_storm_variation.max
-			UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.min = mod.original_storm_variation.min
-		end
-	
-		-- Return result
-		return unit, go_id
 	end
+	-- Changes end. ---------------------------------
 	
-	-- Original function
-	local unit, go_id = func(self, unit_name, unit_template_name, extension_init_data, position, rotation, material)
-	return unit, go_id
+	return unit, unit_template_name
 end)
 
 -- ##########################################################
